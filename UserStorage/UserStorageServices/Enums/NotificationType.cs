0d8b4e0eb6a9c4607e71ace653bc32fa6067d56e
Evgeny Zmitrovich <zmitr-gen@yandex.ru>
2017-11-18 15:06:55 +0300

    Add code from the gist

---
+++ UserStorage/UserStorageServices/Enums/NotificationType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace UserStorageServices.Enums
+{
+    [XmlType(IncludeInSchema = false)]
+    public enum NotificationType
+    {
+        [XmlEnum("addUser")]
+        AddUser,
+
+        [XmlEnum("deleteUser")]
+        DeleteUser
+    }
+}
---
+++ UserStorage/UserStorageServices/Notifications/NotificationContainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace UserStorageServices.Notifications
+{
+    [XmlRoot("NotificationContainer", IsNullable = false, Namespace = "http://tempuri.org/userService/notification")]
+    public class NotificationContainer
+    {
+        [XmlArray("notifications")]
+        [XmlArrayItem("notification")]
+        public Notification[] Notifications { get; set; }
+    }
+}
